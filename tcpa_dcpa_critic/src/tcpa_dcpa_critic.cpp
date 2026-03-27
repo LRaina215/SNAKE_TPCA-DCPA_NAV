6805a4aa8fcf861f26cd84de4a8278197a11cf6d
LRaina215 <20243007059@hainanu.edu.cn>
2026-03-27 22:41:41 +0800

    TCPA插件升级

--- tcpa_dcpa_critic/src/tcpa_dcpa_critic.cpp
+++ tcpa_dcpa_critic/src/tcpa_dcpa_critic.cpp
@@ -10,6 +10,16 @@
 namespace tcpa_dcpa_critic
 {
 
+namespace
+{
+
+double durationToSeconds(const builtin_interfaces::msg::Duration & duration)
+{
+  return static_cast<double>(duration.sec) + (static_cast<double>(duration.nanosec) * 1e-9);
+}
+
+}  // namespace
+
 void TCPADCPACritic::onInit()
 {
   auto node = node_.lock();
@@ -63,39 +73,46 @@ double TCPADCPACritic::scoreTrajectory(const dwb_msgs::msg::Trajectory2D & traj)
 
   const double robot_vx = traj.velocity.x;
   const double robot_vy = traj.velocity.y;
-  const double robot_px = traj.poses.front().x;
-  const double robot_py = traj.poses.front().y;
   const double sigma_safe_sq = sigma_safe_ * sigma_safe_;
   double total_cost = 0.0;
 
   for (const auto & obstacle : obstacles) {
-    const double rel_px = obstacle.position.x - robot_px;
-    const double rel_py = obstacle.position.y - robot_py;
     const double rel_vx = robot_vx - obstacle.velocity.x;
     const double rel_vy = robot_vy - obstacle.velocity.y;
-
-    const double approach_trend = (rel_px * rel_vx) + (rel_py * rel_vy);
-    if (approach_trend <= 0.0) {
-      continue;
-    }
-
     const double rel_v_sq = (rel_vx * rel_vx) + (rel_vy * rel_vy);
     if (rel_v_sq <= 1e-9) {
       continue;
     }
 
-    const double tcpa = approach_trend / rel_v_sq;
-    if (tcpa > tau_safe_) {
-      continue;
+    for (std::size_t i = 0; i < traj.poses.size(); ++i) {
+      const double sample_time =
+        (i < traj.time_offsets.size()) ? durationToSeconds(traj.time_offsets[i]) : 0.0;
+
+      const double robot_px = traj.poses[i].x;
+      const double robot_py = traj.poses[i].y;
+      const double obstacle_px = obstacle.position.x + (obstacle.velocity.x * sample_time);
+      const double obstacle_py = obstacle.position.y + (obstacle.velocity.y * sample_time);
+
+      const double rel_px = obstacle_px - robot_px;
+      const double rel_py = obstacle_py - robot_py;
+      const double approach_trend = (rel_px * rel_vx) + (rel_py * rel_vy);
+      if (approach_trend <= 0.0) {
+        continue;
+      }
+
+      const double tcpa = approach_trend / rel_v_sq;
+      if (tcpa > tau_safe_) {
+        continue;
+      }
+
+      const double dcpa_x = rel_px - (tcpa * rel_vx);
+      const double dcpa_y = rel_py - (tcpa * rel_vy);
+      const double dcpa_sq = (dcpa_x * dcpa_x) + (dcpa_y * dcpa_y);
+
+      const double temporal_term = std::exp(-tcpa / tau_safe_);
+      const double spatial_term = std::exp(-dcpa_sq / (2.0 * sigma_safe_sq));
+      total_cost += temporal_term * spatial_term;
     }
-
-    const double dcpa_x = rel_px - (tcpa * rel_vx);
-    const double dcpa_y = rel_py - (tcpa * rel_vy);
-    const double dcpa_sq = (dcpa_x * dcpa_x) + (dcpa_y * dcpa_y);
-
-    const double temporal_term = std::exp(-tcpa / tau_safe_);
-    const double spatial_term = std::exp(-dcpa_sq / (2.0 * sigma_safe_sq));
-    total_cost += temporal_term * spatial_term;
   }
 
   return total_cost * scale_;
